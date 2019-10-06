a987c26a367efc6041ba8da7c09fcd63d4e2a620
fmvilas <fmvilas@gmail.com>
2019-10-06 14:32:03 +0200

    Parse x-www-url-encoded response in accept-ride lambda

--- lambda/accept-ride.js
+++ lambda/accept-ride.js
@@ -1,9 +1,11 @@
 const dotenv = require('dotenv');
 const { Kafka } = require('kafkajs');
+const qs = require('qs');
 
 dotenv.config();
 
 async function handler(event, context, callback) {
+  event.body = qs.parse(event.body);
   console.log(require('util').inspect(event, { depth: null }));
   console.error(require('util').inspect(event, {depth: null}));
   const client = new Kafka({
@@ -21,7 +23,7 @@ async function handler(event, context, callback) {
   const producer = client.producer();
 
   try {
-    const callbackId = event.callback_id.split('_');
+    const callbackId = event.body.callback_id.split('_');
     const rideId = Number(callbackId[callbackId.length - 1]);
     await producer.connect();
     await producer.send({
@@ -29,7 +31,7 @@ async function handler(event, context, callback) {
       messages: [{
         value: JSON.stringify({
           rideId,
-          driverId: event.user.id,
+          driverId: event.body.user.id,
         })
       }],
     });
