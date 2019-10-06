71eba73a0d08858b905e0c28802a7a7bf02ce7cd
fmvilas <fmvilas@gmail.com>
2019-10-06 13:32:51 +0200

    Add Kafka lambda

---
+++ lambda/publish-kafka.js
@@ -0,0 +1,39 @@
+const dotenv = require('dotenv');
+const { Kafka } = require('kafkajs');
+
+dotenv.config();
+
+async function handler(event, context, callback) {
+  const client = new Kafka({
+    brokers: [process.env.KAFKA_HOST],
+    clientId: 'ABC',
+    ssl: {
+      rejectUnauthorized: true
+    },
+    sasl: {
+      mechanism: 'plain',
+      username: process.env.KAFKA_USERNAME,
+      password: process.env.KAFKA_PASSWORD,
+    },
+  });
+  const producer = client.producer();
+
+  try {
+    await producer.connect();
+    await producer.send({
+      topic: event.topic,
+      messages: event.messages,
+    });
+    callback(null, {
+      statusCode: 200,
+      body: 'OK'
+    });
+  } catch (e) {
+    callback(e, {
+      statusCode: 500,
+      body: 'Unexpected Error'
+    });
+  }
+}
+
+module.exports.handler = handler;
