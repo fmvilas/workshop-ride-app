4cf0f11973ab4ae41a6daea1ba756fee2047ed39
fmvilas <fmvilas@gmail.com>
2019-10-29 11:54:25 +0100

    Add responses to add-slack-user fn

--- lambda/add-slack-user.js
+++ lambda/add-slack-user.js
@@ -7,7 +7,52 @@ dotenv.config();
 async function handler(event, context, callback) {
   try {
     event.body = qs.parse(event.body);
-    console.error(require('util').inspect(event.body, { depth: null }));
+    // { token: 'xxxxxxxxxxxxxxxxxxx',
+    // team_id: 'T34F2JRQU',
+    // team_domain: 'asyncapi',
+    // channel_id: 'GP2DTFU4D',
+    // channel_name: 'privategroup',
+    // user_id: 'U34F2JRRS',
+    // user_name: 'fmvilas',
+    // command: '/ride',
+    // text: 'me as driver',
+    // response_url: 'https://hooks.slack.com/commands/T34F2JRQU/801549252499/xxxxxxxxxxxx',
+    // trigger_id: '812555514372.106512637844.19b1b3760ca9e2e3938177b348049433' }
+
+    const { command, text, user_name, user_id, response_url } = event.body;
+    if (command !== '/ride') {
+      return callback(null, {
+        statusCode: 200,
+        body: 'OK',
+      });
+    }
+
+    if (!text.match(/^me as (driver|user)$/)) {
+      try {
+        await axios.post(response_url, {
+          text: 'I could not understand what you said. Usage is `/ride me as [user/driver]`.',
+          response_type: 'ephemeral',
+        });
+      } catch (e) {
+        return callback(e);
+      }
+
+      return callback(null, {
+        statusCode: 200,
+        body: 'OK',
+      });
+    }
+
+    const type = text.split(' ')[2];
+
+    try {
+      await axios.post(response_url, {
+        text: `Welcome to Ride! You've been added as a ${type}.`,
+        response_type: 'ephemeral',
+      });
+    } catch (e) {
+      return callback(e);
+    }
     
     const client = new Kafka({
       brokers: [process.env.KAFKA_HOST],
@@ -23,7 +68,6 @@ async function handler(event, context, callback) {
     });
 
     const producer = client.producer();  
-    const callbackId = event.body.payload.callback_id.split('_');
     const rideId = Number(callbackId[callbackId.length - 1]);
     await producer.connect();
     await producer.send({
@@ -35,6 +79,7 @@ async function handler(event, context, callback) {
         })
       }],
     });
+
     callback(null, {
       statusCode: 200,
       body: 'OK',
