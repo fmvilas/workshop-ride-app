488c0e5dbd5e49d0b6cd6241b27ea8129e48b4a4
fmvilas <fmvilas@gmail.com>
2019-11-23 22:03:38 +0100

    Add map directions function

---
+++ lambda/map-directions.js
@@ -0,0 +1,36 @@
+const dotenv = require('dotenv');
+const axios = require('axios');
+
+dotenv.config();
+
+async function handler(event, context) {
+  try {
+    const qs = event.queryStringParameters;
+    const gmapsDirectionsApiUrl = `https://maps.googleapis.com/maps/api/directions/json?origin=Grodno&destination=Minsk&mode=driving&key=${process.env.GMAPS_KEY}`;
+
+    await axios({
+      url: gmapsDirectionsApiUrl,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        callback(null, {
+          statusCode: response.status,
+          body: JSON.stringify(response.data)
+        });
+      }).catch((error) => {
+        console.log(error);
+        callback(error);
+      });
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 500,
+      body: e.message,
+    };
+  }
+}
+
+module.exports.handler = handler;
